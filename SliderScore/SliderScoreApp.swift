195bee53c7d2516b91a74ea0b0112e68786d44c9
AlexeyB163 <sourceaisa@mail.ru>
2022-04-12 16:18:31 +0400

    Initial Commit

---
+++ SliderScore/SliderScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderScoreApp.swift
+//  SliderScore
+//
+//  Created by User on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
